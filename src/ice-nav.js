4d7c24fac96cb2954cdf4fd70ffa19ba5649bb85
Andre Jon Branchizio <andrejbranch@gmail.com>
2015-03-17 15:55:30 -0500

    allow fade in as option, allow for user defined function for showing or hiding an item or group

--- src/ice-nav.js
+++ src/ice-nav.js
@@ -5,20 +5,20 @@ angular.module('ajb.ice-nav', [])
         <div id=\"ice-nav-container\">\
             <div class=\"inner\">\
                 <ul class=\"group-list\">\
-                    <li class=\"group\" ng-repeat=\"group in groups\">\
+                    <li class=\"group\" ng-repeat=\"group in groups\" ng-if=\"shouldShow(group)\">\
                         <a class=\"name\">{{group.name}}<\/a>\
                         <table class=\"group-children-container\" cellpadding=\"{{getCellPadding(group)}}\" cellspacing=\"0\">\
                             <tr>\
-                                <td ng-repeat=\"subGroup in group.subGroups\" ng-if=\"group.subGroups.length != 0\">\
+                                <td ng-repeat=\"subGroup in group.subGroups\" ng-if=\"group.subGroups.length != 0\" ng-if=\"shouldShow(subGroup)\">\
                                     <ul class=\"child-list\"> <li class=\"sub-group-name\">{{subGroup.name}}<\/li>\
-                                        <li ng-click=\"onClick(item)\" class=\"child\" ng-repeat=\"item in subGroup.items\">\
+                                        <li ng-click=\"onClick(item)\" class=\"child\" ng-repeat=\"item in subGroup.items\" ng-if=\"shouldShow(item)\">\
                                             <a>{{item.name}}<\/a>\
                                         <\/li>\
                                     <\/ul>\
                                 <\/td>\
                                 <td ng-if=\"group.items.length != 0\">\
                                     <ul class=\"child-list\">\
-                                        <li ng-click=\"onClick(item)\" class=\"child\" ng-repeat=\"item in group.items\">\
+                                        <li ng-click=\"onClick(item)\" class=\"child\" ng-repeat=\"item in group.items\" ng-if=\"shouldShow(item)\">\
                                             <a>{{item.name}}<\/a>\
                                         <\/li>\
                                     <\/ul>\
@@ -44,8 +44,21 @@ angular.module('ajb.ice-nav', [])
             $scope.onClick = function (item) {
                 window.location = item.link
             }
+
+            $scope.shouldShow = function (group) {
+
+                if (typeof $scope.isHidden == 'function') {
+                    return !$scope.isHidden(group)
+                }
+
+                return true
+            }
+
+            if ($scope.fade) {
+                element.find('#ice-nav-container').addClass('fade')
+            }
         },
-        scope: { groups: "="},
-        templateUrl: "ajb-iceNav.html"
+        scope: { groups: "=", isHidden: "=", fade: "=" },
+        templateUrl: "ajb-iceNav.html",
     }
 })
